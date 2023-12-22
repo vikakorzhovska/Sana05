50c970bdfa5ab0b96cd88a1d9eecac63eb7093cd
Вікторія Коржовська <ipz234_kvv@student.ztu.edu.ua>
2023-12-22 13:53:44 +0200

    Created class MyDate

---
+++ ConsoleApp1/MyDate.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class MyDate
+    {
+        protected int year;
+        protected int month;
+        protected int day;
+        protected int hours;
+        protected int minutes;
+
+        public MyDate()
+        {
+            year = 1000;
+            month = 10;
+            day = 10;
+            hours = 10;
+            minutes = 10;
+        }
+        public MyDate(int year, int month, int day, int hours, int minutes)
+        {
+            this.year = year;
+            this.month = month;
+            this.day = day;
+            this.hours = hours;
+            this.minutes = minutes;
+        }
+        public MyDate(MyDate other)
+        {
+            this.year = other.year;
+            this.month = other.month;
+            this.day = other.day;
+            this.hours = other.hours;
+            this.minutes = other.minutes;
+        }
+        public int getYear()
+        {
+            return year;
+        }
+        public int getMonth()
+        {
+            return month;
+        }
+        public int getDay()
+        {
+            return day;
+        }
+        public int getHours()
+        {
+            return hours;
+        }
+        public int getMinutes()
+        {
+            return minutes;
+        }
+        public void setYear(int year)
+        {
+            this.year = year;
+        }
+        public void setMonth(int month)
+        {
+            if (month > 0 && month <= 12)
+                this.month = month;
+            else
+                throw new Exception("Немає такого місяця");
+        }
+        public void setDay(int day)
+        {
+            if (day > 0 && day <= 31)
+                this.day = day;
+            else
+                throw new Exception("Немає такого дня");
+        }
+        public void setHours(int hours)
+        {
+            if (hours >= 0 && hours <= 23)
+                this.hours = hours;
+            else
+                throw new Exception("Не правильно введене значення");
+        }
+        public void setMinutes(int minutes)
+        {
+            if (minutes >= 0 && minutes <= 59)
+                this.minutes = minutes;
+            else
+                throw new Exception("Не правильно введене значення");
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using ConsoleApp1;
+using System.Text;
+
+Console.InputEncoding = Encoding.Unicode;
+Console.OutputEncoding = Encoding.Unicode;
+MyDate date1 = new MyDate();
+Console.WriteLine($"Дата за замовчуванням: {date1.getYear()}.{date1.getMonth()}.{date1.getDay()}  {date1.getHours()}:{date1.getMinutes()}");
+
+MyDate date2 = new MyDate(2005, 12, 29, 12, 30);
+Console.WriteLine($"Дата з параметрами: {date2.getYear()}.{date2.getMonth()}.{date2.getDay()}  {date2.getHours()}:{date2.getMinutes()}");
+
+MyDate date3 = new MyDate(date2);
+Console.WriteLine($"Скопійована дата: {date3.getYear()}.{date3.getMonth()}.{date3.getDay()}   {date3.getHours()}:{date3.getMinutes()}");
